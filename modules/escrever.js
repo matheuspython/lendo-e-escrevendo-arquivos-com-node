5d6b0d4af8dfbf47f216fb3b6d1a4eed1c086e13
matheus souza <ruivorobado@gmail.com>
2021-09-20 22:00:14 -0300

    criando funcionalidade que transforma obj em json

--- modules/escrever.js
+++ modules/escrever.js
@@ -1,9 +1,17 @@
 const fs = require("fs").promises
 const path = require("path")
-const caminhoArquivo = path.resolve(__dirname, '..', 'teste.txt')
+const caminhoArquivo = path.resolve(__dirname, '..', 'teste.json')
 
-  
+const pessoas = [
+  {nome: 'joão'},
+  {nome: 'maria'},
+  {nome: 'eduardo'},
+  {nome: 'luiza'}
+]
+const json = JSON.stringify(pessoas,'',2)//converte o obj em json
 
-fs.writeFile(caminhoArquivo,'frase \n', {flag: 'a', encoding: 'utf-8'})
+
+
+fs.writeFile(caminhoArquivo,json, {flag: 'a', encoding: 'utf-8'})
 //a flag w apaga tudo que esta no arquivo caso ele ja exista
 //caso vc queira fazer um "append" utilize a flag:'a'
